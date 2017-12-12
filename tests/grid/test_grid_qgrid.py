ef489358d438c80ddc43a32636230b9683e91596
Tim Paine <t.paine154@gmail.com>
2017-12-12 04:04:52 +0000

    adding grid tests

---
+++ tests/grid/test_grid_qgrid.py
@@ -0,0 +1,29 @@
+from mock import patch
+
+
+class TestConfig:
+    def setup(self):
+        pass
+        # setup() before each test method
+
+    def teardown(self):
+        pass
+        # teardown() after each test method
+
+    @classmethod
+    def setup_class(cls):
+        pass
+        # setup_class() before any methods in this class
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+        # teardown_class() after any methods in this class
+
+    def test_grid(self):
+        with patch('lantern.grids.grid_qgrid.in_ipynb', create=True) as mock1:
+            import lantern as l
+            mock1.return_value = True
+            df = l.bar.sample()
+            l.grid(df)
+
