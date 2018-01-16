32e841e7f8b6b65f56d69e13cdb0038b8e156070
Tim Paine <t.paine154@gmail.com>
2018-01-16 18:00:41 -0500

    .

--- lantern/plotting/plotutils.py
+++ lantern/plotting/plotutils.py
@@ -1,4 +1,3 @@
-from .plottypes import lookup
 import random
 import numpy as np
 
@@ -20,57 +19,58 @@ def get_color(i, col, color):
     return c
 
 
-def _conf(type, colors, x, y, i, col):
-    '''select type and color from their options, allow strings for some'''
-    if isinstance(type, str):
-        typ = lookup(type)
-        if isinstance(colors, list):
-            color = (colors[i:i+1] or [_r()])[0]
-        elif isinstance(colors, dict):
-            color = colors.get(col, _r())
-        elif isinstance(colors, str) and colors:
-            color = colors
-        else:
-            color = _r()
+# def _conf(type, colors, x, y, i, col):
+#     from .plottypes import lookup
+#     '''select type and color from their options, allow strings for some'''
+#     if isinstance(type, str):
+#         typ = lookup(type)
+#         if isinstance(colors, list):
+#             color = (colors[i:i+1] or [_r()])[0]
+#         elif isinstance(colors, dict):
+#             color = colors.get(col, _r())
+#         elif isinstance(colors, str) and colors:
+#             color = colors
+#         else:
+#             color = _r()
 
-    if isinstance(type, list):
-        typ = (type[i:i+1] or ['line'])[0]
-        if isinstance(typ, str):
-            typ = lookup(typ)
-        if isinstance(colors, list):
-            color = (colors[i:i+1] or [_r()])[0]
-        elif isinstance(colors, dict):
-            color = colors.get(col, _r())
-        elif isinstance(colors, str) and colors:
-            color = colors
-        else:
-            color = _r()
+#     if isinstance(type, list):
+#         typ = (type[i:i+1] or ['line'])[0]
+#         if isinstance(typ, str):
+#             typ = lookup(typ)
+#         if isinstance(colors, list):
+#             color = (colors[i:i+1] or [_r()])[0]
+#         elif isinstance(colors, dict):
+#             color = colors.get(col, _r())
+#         elif isinstance(colors, str) and colors:
+#             color = colors
+#         else:
+#             color = _r()
 
-    elif isinstance(type, dict):
-        typ = type.get(col, 'line')
-        if isinstance(type.get(col, 'line'), str):
-            typ = lookup(typ)
+#     elif isinstance(type, dict):
+#         typ = type.get(col, 'line')
+#         if isinstance(type.get(col, 'line'), str):
+#             typ = lookup(typ)
 
-        if isinstance(colors, list):
-            color = (colors[i:i+1] or [_r()])[0]
-        elif isinstance(colors, dict):
-            color = colors.get(col, _r())
-        elif isinstance(colors, str):
-            color = colors
-        else:
-            color = _r()
+#         if isinstance(colors, list):
+#             color = (colors[i:i+1] or [_r()])[0]
+#         elif isinstance(colors, dict):
+#             color = colors.get(col, _r())
+#         elif isinstance(colors, str):
+#             color = colors
+#         else:
+#             color = _r()
 
-    if y and isinstance(y, dict):
-        y = y.get(col, 'left')
-    else:
-        y = 'left'
+#     if y and isinstance(y, dict):
+#         y = y.get(col, 'left')
+#     else:
+#         y = 'left'
 
-    if x and isinstance(x, dict):
-        x = x.get(col, 'left')
-    else:
-        x = 'bottom'
+#     if x and isinstance(x, dict):
+#         x = x.get(col, 'left')
+#     else:
+#         x = 'bottom'
 
-    return typ, color, x, y
+#     return typ, color, x, y
 
 
 def _parseScatter(kwargs, col):
