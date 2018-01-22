eaebb5ceda9cf7c9d5c0e94191c6a5200f657bc9
Tim Paine <t.paine154@gmail.com>
2018-01-22 01:27:15 -0500

    .

--- lantern/live/test.py
+++ lantern/live/test.py
@@ -5,7 +5,7 @@ from .base import run
 
 
 if __name__ == '__main__':
-    # logging.basicConfig(level=logging.INFO)
+    # loggingt st.basicConfig(level=logging.INFO)
     s = WebSocket("wss://ws-feed.gdax.com")
     x = run(s)
     print(x)
