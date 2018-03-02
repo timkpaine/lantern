5b21bba182d1b1491ad807baede3bfcfa5bbcada
Tim Paine <t.paine154@gmail.com>
2018-03-01 21:48:56 -0500

    working on #155

--- lantern/__init__.py
+++ lantern/__init__.py
@@ -1,7 +1,7 @@
 from .plotting import plot, figure
 from .data import *
 from .grids import grid
-from .live import run, LanternLive, Streaming, WebSocketSource, RandomSource, RandomSource2
+from .live import run, pipeline, LanternLive, Streaming, WebSocketSource, RandomSource, RandomSource2
 
 
 __all__ = ['plot', 'figure', 'grids', 'data', 'extensions']
