fef5dd925db7089e3c158e28372d8e079716e922
Tim Paine <t.paine154@gmail.com>
2018-03-05 09:55:03 -0500

    upping version for release

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pylantern',
-    version='0.0.16',
+    version='0.0.17',
     description='Analytics library',
     long_description=long_description,
     url='https://github.com/timkpaine/lantern',
-    download_url='https://github.com/timkpaine/lantern/archive/v0.0.16.tar.gz',
+    download_url='https://github.com/timkpaine/lantern/archive/v0.0.17.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='LGPL',
