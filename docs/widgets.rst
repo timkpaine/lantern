88bd0ba66c2995f85fd52a725e8fce7ecbebc67f
Tim Paine <t.paine154@gmail.com>
2018-09-26 15:49:01 -0400

    adding sidebar foo

--- docs/widgets.rst
+++ docs/widgets.rst
@@ -2,6 +2,11 @@
 Widget Tools
 ==============
 
+Sidebar Wrapper
+===============
+.. method:: lantern.sidebar(<stuff to display in sidebar>)
+
+
 Variable Inspector
 ===================
 .. method:: lantern.VariableInspector()
--- lantern/widgets/__init__.py
+++ lantern/widgets/__init__.py
@@ -1,1 +1,9 @@
+from sidecar import Sidecar
+from IPython.display import display
 from .variable_inspector import VariableInspector
+
+
+def sidebar(stuff, title='Sidebar'):
+    sc = Sidecar(title=title)
+    with sc:
+        display(stuff)
